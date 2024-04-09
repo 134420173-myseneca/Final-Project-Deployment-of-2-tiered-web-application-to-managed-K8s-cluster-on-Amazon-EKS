df55835778f198e460fab32a2bfb5736cad7c5ec
Rabia Khalid <39117847+134420173-myseneca@users.noreply.github.com>
2024-04-09 08:02:48 -0400

    adds download function to get image url from s3

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 from pymysql import connections
 import os
 import random
 import argparse
-
+import boto3
+import logging
+import botocore
 
 app = Flask(__name__)
 
@@ -12,19 +14,21 @@ DBUSER = os.environ.get("DBUSER") or "root"
 DBPWD = os.environ.get("DBPWD") or "passwors"
 DATABASE = os.environ.get("DATABASE") or "employees"
 COLOR_FROM_ENV = os.environ.get('APP_COLOR') or "lime"
-DBPORT = int(os.environ.get("DBPORT"))
+BACKGROUND_IMAGE = os.environ.get("BACKGROUND_IMAGE") or "Invalid Image been passed"
+GROUP_NAME = os.environ.get('GROUP_NAME') or "GROUP9"
+#DBPORT = int(os.environ.get("DBPORT"))
 
 # Create a connection to the MySQL database
-db_conn = connections.Connection(
-    host= DBHOST,
-    port=DBPORT,
-    user= DBUSER,
-    password= DBPWD, 
-    db= DATABASE
+# db_conn = connections.Connection(
+#     host= DBHOST,
+#     port=DBPORT,
+#     user= DBUSER,
+#     password= DBPWD, 
+#     db= DATABASE
     
-)
-output = {}
-table = 'employee';
+# )
+# output = {}
+# table = 'employee';
 
 # Define the supported color codes
 color_codes = {
@@ -44,14 +48,128 @@ SUPPORTED_COLORS = ",".join(color_codes.keys())
 # Generate a random color
 COLOR = random.choice(["red", "green", "blue", "blue2", "darkblue", "pink", "lime"])
 
+# Read the list of existing buckets
+# def list_bucket():
+#     # Create bucket
+#     try:
+#         s3 = boto3.client('s3')
+#         response = s3.list_buckets()
+#         if response:
+#             print('Buckets exists..')
+#             for bucket in response['Buckets']:
+#                 print(f'  {bucket["Name"]}')
+#     except Exception as e:
+#         logging.error(e)
+#         return False
+#     return True
+    
+#     list_bucket()
+
+
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
     return render_template('addemp.html', color=color_codes[COLOR])
+    
+from flask import render_template
+import boto3
+import logging
+
+from flask import render_template
 
 @app.route("/about", methods=['GET','POST'])
 def about():
-    return render_template('about.html', color=color_codes[COLOR])
+    try:
+        s3 = boto3.client('s3')
+        response = s3.list_buckets()
+        if response:
+            buckets = [bucket["Name"] for bucket in response['Buckets']]
+            print('Buckets exist..')
+            for bucket in buckets:
+                print(f'  {bucket}')
+    except Exception as e:
+        logging.error(e)
+        return render_template('error.html', error_message="Error occurred while fetching buckets")  # Return an error template or handle the error appropriately
+
+    # Call the download function to get the image URL
+    image_url = download("background_image.png")  # Replace "background_image.png" with the actual file name in S3
+    
+    # Assuming you have defined color_codes and COLOR somewhere in your code
+    color = color_codes[COLOR]
+
+    return render_template('about.html', buckets=buckets, color=color, background_image=image_url)
+
+# Define the download function to get the S3 URL of the image
+def download(object_name):
+    try:
+        s3 = boto3.client('s3')
+        bucket_name = 'clo835-group9'  # Replace 'your_bucket_name' with the actual bucket name
+        #https://clo835-group9.s3.amazonaws.com/background_image.png
+        image_url = f"https://{bucket_name}.s3.amazonaws.com/{object_name}"
+        #prints the bucket name which is clo835-group9
+        print(bucket_name) 
+        #prints the image name which is background_image.png
+        print(bucket_name) 
+        print(object_name)  
+        print("Background Image Location ---> " + image_url) # Added for Logging of Background Image Path
+        return image_url
+    except Exception as e:
+        logging.error(e)
+        return None
+
+
+# @app.route("/about", methods=['GET','POST'])
+# def about():
+#     try:
+#         s3 = boto3.client('s3')
+#         response = s3.list_buckets()
+#         if response:
+#             buckets = [bucket["Name"] for bucket in response['Buckets']]
+#             print('Buckets exist..')
+#             for bucket in buckets:
+#                 print(f'  {bucket}')
+#     except Exception as e:
+#         logging.error(e)
+#         return render_template('error.html', error_message="Error occurred while fetching buckets")  # Return an error template or handle the error appropriately
+
+#     # Assuming you have defined color_codes and COLOR somewhere in your code
+#     return render_template('about.html', buckets=buckets, color=color_codes[COLOR])
+    
+#     return render_template('about.html', background_image = image_url, group_name = GROUP_NAME)
+# # @app.route("/download", methods=['GET','POST'])    
+# # def download_file(file_name, bucket):
+# #     # """
+# #     # Function to download a given file from an S3 bucket
+# #     # """
+    
+# #     s3 = boto3.resource('s3')
+# #     output = f"downloads/{file_name}"
+# #     s3.Bucket(bucket).download_file(file_name, output)
+
+# #     return output
+    
+# def download(image_url):
+#   try:
+#          bucket = image_url.split('//')[1].split('.')[0]
+#          object_name = '/'.join(image_url.split('//')[1].split('/')[1:])
+#          print(bucket)  # prints 'privatebucketclo835'
+#          print(object_name)  # prints 'minionparty.png'
+#          print("Background Image Location --->" + image_url) # Added for Logging of Background Image Path
+#          s3 = boto3.resource('s3')
+#          output_dir = "static"
+#          if not os.path.exists(output_dir):
+#                  os.makedirs(output_dir)
+#          output = os.path.join(output_dir, "background_image.png")
+#          s3.Bucket(bucket).download_file(object_name, output)
+
+#          return output
+
+#   except botocore.exceptions.ClientError as e:
+#                 if e.response['Error']['Code'] == "404":
+#                     print("The object does not exist.")
+#                 else:
+#                     raise
+
     
 @app.route("/addemp", methods=['POST'])
 def AddEmp():
@@ -79,7 +197,10 @@ def AddEmp():
 
 @app.route("/getemp", methods=['GET', 'POST'])
 def GetEmp():
-    return render_template("getemp.html", color=color_codes[COLOR])
+    
+        image_url = url_for('static', filename='background_image.png')
+
+        return render_template("getemp.html", background_image = image_url, color=color_codes[COLOR])
 
 
 @app.route("/fetchdata", methods=['GET','POST'])
@@ -133,4 +254,4 @@ if __name__ == '__main__':
         print("Color not supported. Received '" + COLOR + "' expected one of " + SUPPORTED_COLORS)
         exit(1)
 
-    app.run(host='0.0.0.0',port=8080,debug=True)
+    app.run(host='0.0.0.0',port=8081,debug=True)
